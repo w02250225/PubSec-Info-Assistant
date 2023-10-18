4e9a2970c09014308f44b79d59eb3bdaae39fbae
Taylor <TaylorN15@users.noreply.github.com>
2023-10-19 07:49:23 +1000

    Added GPT4 web app to deployment script

--- scripts/deploy-webapp.sh
+++ scripts/deploy-webapp.sh
@@ -49,7 +49,7 @@ fi
 # deploy the zip file to the webapp
 az webapp deploy --name $AZURE_WEBAPP_NAME --resource-group $RESOURCE_GROUP_NAME --type zip --src-path webapp.zip --async true --timeout 600000 --verbose
 
-$AZURE_WEBAPP_NAME_GPT4 = "${AZURE_WEBAPP_NAME/web/web-gpt4}"
+AZURE_WEBAPP_NAME_GPT4="${AZURE_WEBAPP_NAME/web/web-gpt4}"
 az webapp deploy --name $AZURE_WEBAPP_NAME_GPT4 --resource-group $RESOURCE_GROUP_NAME --type zip --src-path webapp.zip --async true --timeout 600000 --verbose
 
 echo "Webapp deployed successfully"
