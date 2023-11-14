887d122c31c57dd7e8d9dddf53cf289fd72b9673
Taylor <TaylorN15@users.noreply.github.com>
2023-11-14 02:52:28 +0000

    Updated scripts

---
+++ scripts/build-functions.sh
@@ -0,0 +1,26 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+#!/bin/bash
+set -e
+
+figlet Build
+
+# Get the directory that this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "${DIR}"/../scripts/load-env.sh
+BINARIES_OUTPUT_PATH="${DIR}/../artifacts/build/"
+FUNCTIONS_ROOT_PATH="${DIR}/../functions"
+
+# reset the current directory on exit using a trap so that the directory is reset even on error
+#function finish {
+#  popd > /dev/null
+#}
+#trap finish EXIT
+
+# Clean previous runs on a dev machine
+rm -rf ${BINARIES_OUTPUT_PATH} && mkdir -p ${BINARIES_OUTPUT_PATH}
+
+# Build the Azure Functions
+cd ${FUNCTIONS_ROOT_PATH}
+zip -r ${BINARIES_OUTPUT_PATH}/functions.zip . -x ".venv/*" 2>&1 | pv > /dev/null
