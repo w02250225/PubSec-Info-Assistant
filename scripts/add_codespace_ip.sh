ae32a54632a8a2f11a0a0101ddeccf4e652fa8e0
Taylor <TaylorN15@users.noreply.github.com>
2024-01-11 06:52:43 +0000

    Other workspace changes

--- scripts/add_codespace_ip.sh
+++ scripts/add_codespace_ip.sh
@@ -12,11 +12,11 @@ CURR_IP_ADDR=$(curl -s ifconfig.me)
 # Compare the old and current IP address
 if [ "$OLD_IP_ADDR" != "$CURR_IP_ADDR" ]; then
 
-    az webapp config access-restriction remove -g rgdsibinfoasstazs01 -n infoasst-web-kr839 --rule-name Codespace
-    az webapp config access-restriction add -g rgdsibinfoasstazs01 -n infoasst-web-kr839 --rule-name Codespace --action Allow --ip-address $CURR_IP_ADDR --priority 250
+    az webapp config access-restriction remove -g rgdsibinfoasstazs01 -n infoasst-web-kr839 --rule-name Codespace || true
+    az webapp config access-restriction add -g rgdsibinfoasstazs01 -n infoasst-web-kr839 --rule-name Codespace --action Allow --ip-address $CURR_IP_ADDR --priority 250 || true
 
-    az webapp config access-restriction remove -g rgdsibinfoasstazs01 -n infoasst-enrichmentweb-kr839 --rule-name Codespace
-    az webapp config access-restriction add -g rgdsibinfoasstazs01 -n infoasst-enrichmentweb-kr839 --rule-name Codespace --action Allow --ip-address $CURR_IP_ADDR --priority 250
+    az webapp config access-restriction remove -g rgdsibinfoasstazs01 -n infoasst-enrichmentweb-kr839 --rule-name Codespace || true
+    az webapp config access-restriction add -g rgdsibinfoasstazs01 -n infoasst-enrichmentweb-kr839 --rule-name Codespace --action Allow --ip-address $CURR_IP_ADDR --priority 250 || true
 
     # Attempt to remove the old IP address rule and continue on failure
     echo "Removing old IP address rule for IP: $OLD_IP_ADDR on CADISOPENAIAZS01"
@@ -31,7 +31,7 @@ if [ "$OLD_IP_ADDR" != "$CURR_IP_ADDR" ]; then
     
     # Add the new IP address rule
     echo "Adding new IP address rule for IP: $CURR_IP_ADDR on CADISOPENAIAZS01"
-    az cognitiveservices account network-rule add --name CADISOPENAIAZS01 --resource-group RGDOPENAIAZS01 --ip-address $CURR_IP_ADDR
+    az cognitiveservices account network-rule add --name CADISOPENAIAZS01 --resource-group RGDOPENAIAZS01 --ip-address $CURR_IP_ADDR || true
     
     # echo "Adding new IP address rule for IP: $CURR_IP_ADDR on CADISOPENAIEUS01"
     # az cognitiveservices account network-rule add --name CADISOPENAIEUS01 --resource-group RGDOPENAIAZS01 --ip-address $CURR_IP_ADDR
